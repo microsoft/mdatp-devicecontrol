30aeb77e637fa729e20921277e1d05aaabad538d
Josh Bregman <96197424+j0shbregman@users.noreply.github.com>
2024-05-05 16:36:27 -0400

    Added IntuneArgs to Test

--- python/tests/test_dcintune.py
+++ python/tests/test_dcintune.py
@@ -1,5 +1,29 @@
 import mdedevicecontrol.dcintune as intune
 
+import os
+
+class DcIntuneArgs: 
+
+    def __init__(self):
+
+        self.tenantId = os.environ["AAD_TENANT_ID"]
+        self.clientId = os.environ["AAD_CLIENT_ID"]
+        self.loggingConf = doc.file("logging.conf")
+
+
+        self.command = "export"
+
+        self.dest = os.getcwd()
+        self.package_name = "Test1"
+
+        self.templates_path = intune.path_array("templates")
+        self.template = "dcutil.j2"
+        self.readme_template = "readme.j2"
+        self.description_template = "description.j2"
+        self.readme_file = "readme.md"
+        
+        
+
 def test_rule_xml_1():
 
     rule = intune.DeviceControlPolicyTemplate.DeviceControlRule()
