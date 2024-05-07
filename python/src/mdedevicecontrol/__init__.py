c7bdbf0d0697ee3e01b97d4a4141e87892a19256
Josh Bregman <96197424+j0shbregman@users.noreply.github.com>
2024-05-07 19:25:20 +0000

    Added logic to save rules and groups

--- python/src/mdedevicecontrol/__init__.py
+++ python/src/mdedevicecontrol/__init__.py
@@ -2658,3 +2658,21 @@ class api:
                 os.mkdir(layout_path)
 
             path_map[orig_path] = layout_path
+
+            for group in self.groups:
+
+                group_file_path = pathlib.PurePath(os.path.join(path_map[intune.Package.WINDOWS_GROUPS_PATH],group.name+".xml"))
+                group_file = open(group_file_path,"w")
+                group_file.write(str(group))
+                group_file.close()
+
+                logger.info("Exporting group "+group.name+" to "+str(group_file_path))
+
+            for rule in self.rules:
+                
+                rule_file_path = pathlib.PurePath(os.path.join(path_map[intune.Package.WINDOWS_RULES_PATH],rule.name+".xml"))
+                rule_file = open(rule_file_path,"w")
+                rule_file.write(str(rule))
+                rule_file.close()
+
+                logger.info("Exporting rule "+rule.name+" to "+str(rule_file_path))
