555a57aaee67ff98f4166f8fc0152ba148197fd7
Josh Bregman <96197424+j0shbregman@users.noreply.github.com>
2024-05-09 20:53:26 +0000

    Added entry to the list

--- python/tests/test_dcgraph.py
+++ python/tests/test_dcgraph.py
@@ -152,6 +152,10 @@ async def test_v2_group():
         #This is an entry
         rule_data_entry = DeviceManagementConfigurationGroupSettingCollectionInstance()
         rule_data_entry.setting_definition_id = "device_vendor_msft_defender_configuration_devicecontrol_policyrules_{ruleid}_ruledata_entry"
+        
+        #Add it to the list
+        rule_data_group_setting_value_children.append(rule_data_entry)
+        
         rule_data_group_setting_collection_value = DeviceManagementConfigurationGroupSettingValue()
         rule_data_entry.group_setting_collection_value = rule_data_group_setting_collection_value
 
