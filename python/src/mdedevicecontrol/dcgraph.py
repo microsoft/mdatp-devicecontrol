04fc10764a4f66e6daa3e5acdf1c0cec41ff0b57
Josh Bregman <96197424+j0shbregman@users.noreply.github.com>
2024-05-10 02:01:21 +0000

    Added a debugger to graph API

--- python/src/mdedevicecontrol/dcgraph.py
+++ python/src/mdedevicecontrol/dcgraph.py
@@ -2,6 +2,12 @@ from configparser import SectionProxy
 from azure.identity import DeviceCodeCredential
 from azure.identity.aio import ClientSecretCredential
 
+from msgraph_beta import GraphServiceClient, GraphRequestAdapter
+from msgraph_beta import GraphClientFactory
+from kiota_authentication_azure.azure_identity_authentication_provider import (
+    AzureIdentityAuthenticationProvider
+)
+
 import asyncio
 
 from msgraph_beta import GraphServiceClient
@@ -35,6 +41,40 @@ https://graph.microsoft.com/beta/deviceManagement/reusablePolicySettings
 import logging
 logger = logging.getLogger(__name__)
 
+from kiota_http.middleware import BaseMiddleware
+import httpx
+
+class DebugHandler(BaseMiddleware):
+
+    async def send(
+        self, request: httpx.Request, transport: httpx.AsyncBaseTransport
+    ) -> httpx.Response:
+        print()
+        print(f"{request.method} {request.url}")
+        for key, value in request.headers.items():
+            print(f"{key}: {value}")
+        if request.content:
+            print()
+            print("Request body:")
+            print(request.content.decode())
+
+        response: httpx.Response = await super().send(request, transport)
+
+        print()
+        print(f"Response: {response.status_code} {response.reason_phrase}")
+        print("Response headers:")
+        for key, value in response.headers.items():
+            print(f"{key}: {value}")
+
+        print()
+        print("Response body:")
+        response_content = await response.aread()
+        print(f"Response content: {response_content.decode()}")
+
+        return response
+
+
+
 class Graph:
     
     device_code_credential: DeviceCodeCredential
@@ -55,9 +95,21 @@ class Graph:
         logger.debug("scopes: "+str(graph_scopes))
         #self.device_code_credential = DeviceCodeCredential(client_id, tenant_id = tenant_id)
 
+        _middleware = GraphClientFactory.get_default_middleware(None)
+        _middleware.append(DebugHandler())
+        _http_client = GraphClientFactory.create_with_custom_middleware(
+            _middleware
+        )
         self.client_credential = ClientSecretCredential(self.tenant_id, client_id, client_secret)
+        _auth_provider = AzureIdentityAuthenticationProvider(self.client_credential)
+        _adapter = GraphRequestAdapter(_auth_provider, _http_client)
+
         logger.debug("Client credential created.")
-        self.graph_client = GraphServiceClient(self.client_credential)
+        self.graph_client =  GraphServiceClient(
+            self.client_credential_credential,
+            scopes=["https://graph.microsoft.com/.default"],
+            request_adapter=_adapter,
+)
         logger.debug("Graph client created.")
 
     async def get_app_only_token(self):
