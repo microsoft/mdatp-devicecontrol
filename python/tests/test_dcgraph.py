05a24d9c6421ef6b678d3207aade209f64c854d4
Josh Bregman <96197424+j0shbregman@users.noreply.github.com>
2024-05-17 09:18:11 +0000

    Fixed dcv2 rule test

--- python/tests/test_dcgraph.py
+++ python/tests/test_dcgraph.py
@@ -278,7 +278,7 @@ async def test_v2_rule():
         logger.info(str(rule))
 
         try:
-              result = await graph.create_policy_v2(rule)
+              result = await graph.create_policy_v2("test","test rule",[rule])
         except RuntimeError as e:
                logger.error(e)
 
